b2d580e4b5b878fd81a728797db0c871bf5492e0
Yoni Nazarathy <yoninazarathy@>
2016-09-02 17:31:08 +1000

    created new level

---
+++ Square Root Marbles/GameLevelModel.swift
@@ -0,0 +1,14 @@
+//
+//  GameLevelModel.swift
+//  Square Root Marbles
+//
+//  Created by Yoni Nazarathy on 2/09/2016.
+//  Copyright © 2016 oneonepsilon. All rights reserved.
+//
+
+import Foundation
+
+class GameLevelModel{
+    
+    
+}
